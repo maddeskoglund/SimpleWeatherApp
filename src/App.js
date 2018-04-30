a18d7506b00e976f47278bf05ad2c5a29944c627
Max Elander <max.elander@mac.com>
2018-04-30 15:19:12 +0200

    Bytt namn och fixat med props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import Day from './components/Day'
-import Datum from './components/Date'
-
+import React, { Component } from "react";
+import Day from "./components/Day";
+import Datum from "./components/Datum";
 
 // import Background from './components/Background'
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       today: {
         city: [],
@@ -32,16 +31,15 @@ class App extends Component {
         tempMin: [],
         rain: [],
         wind: []
-      },
-    }
+      }
+    };
   }
 
   // error????
 
-
   componentDidMount() {
-    const long = '16.90267';
-    const lat = '59.65584';
+    const long = "16.90267";
+    const lat = "59.65584";
     let tempNow = -Infinity;
 
     const todayTemp = [];
@@ -54,20 +52,27 @@ class App extends Component {
     const tomorrowPlus1Rain = [];
     const tomorrowPlus1Wind = [];
 
-
-    const url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${long}/lat/${lat}/data.json`
+    const url = `https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/${long}/lat/${lat}/data.json`;
     fetch(url)
-      .then((response) => {
-        return response.json()
+      .then(response => {
+        return response.json();
       })
-      .then((data) => {
+      .then(data => {
         console.log(data);
 
         const today = new Date();
+        var options = {
+          weekday: "long",
+          month: "long",
+          day: "numeric"
+        };
         const todayStr = today.toLocaleString().slice(0, 10);
+        const todayDatumStr = today.toLocaleString("sv-SE", options);
         const tomorrow = new Date(today.setDate(today.getDate() + 1));
         const tomorrowStr = tomorrow.toLocaleString().slice(0, 10);
-        const tomorrowPlus1 = new Date(tomorrow.setDate(tomorrow.getDate() + 1));
+        const tomorrowPlus1 = new Date(
+          tomorrow.setDate(tomorrow.getDate() + 1)
+        );
         const tomorrowPlus1Str = tomorrowPlus1.toLocaleString().slice(0, 10);
 
         tempNow = data.timeSeries[0].parameters.filter(
@@ -90,7 +95,6 @@ class App extends Component {
             return element.name === "ws";
           })[0].values[0];
 
-
           if (validTime.startsWith(todayStr)) {
             todayTemp.push(temperature);
             todayRain.push(rainfall);
@@ -104,24 +108,37 @@ class App extends Component {
             tomorrowPlus1Rain.push(rainfall);
             tomorrowPlus1Wind.push(windspeed);
           }
+        });
 
-        })
+        const todayTempMax = Math.max(...todayTemp);
+        const todayTempMin = Math.min(...todayTemp);
+        const tomorrowTempMax = Math.max(...tomorrowTemp);
+        const tomorrowTempMin = Math.min(...tomorrowTemp);
+        const tomorrowPlus1TempMax = Math.max(...tomorrowPlus1Temp);
+        const tomorrowPlus1TempMin = Math.min(...tomorrowPlus1Temp);
+        const sumTodayRain = Math.round(
+          todayRain.reduce((total, amount) => total + amount)
+        );
+        const sumTomorrowRain = Math.round(
+          tomorrowRain.reduce((total, amount) => total + amount)
+        );
+        const sumTomorrowPlus1Rain = Math.round(
+          tomorrowPlus1Rain.reduce((total, amount) => total + amount)
+        );
+        const avTodayWind = Math.round(
+          todayWind.reduce((total, amount) => total + amount) / todayWind.length
+        );
+        const avTomorrowWind = Math.round(
+          tomorrowWind.reduce((total, amount) => total + amount) /
+            tomorrowWind.length
+        );
+        const avTomorrowPlus1Wind = Math.round(
+          tomorrowPlus1Wind.reduce((total, amount) => total + amount) /
+            tomorrowPlus1Wind.length
+        );
 
-        const todayTempMax = Math.max(...todayTemp)
-        const todayTempMin = Math.min(...todayTemp)
-        const tomorrowTempMax = Math.max(...tomorrowTemp)
-        const tomorrowTempMin = Math.min(...tomorrowTemp)
-        const tomorrowPlus1TempMax = Math.max(...tomorrowPlus1Temp)
-        const tomorrowPlus1TempMin = Math.min(...tomorrowPlus1Temp)
-        const sumTodayRain = Math.round(todayRain.reduce((total, amount) => total + amount));
-        const sumTomorrowRain = Math.round(tomorrowRain.reduce((total, amount) => total + amount));
-        const sumTomorrowPlus1Rain = Math.round(tomorrowPlus1Rain.reduce((total, amount) => total + amount));
-        const avTodayWind = Math.round(todayWind.reduce((total, amount) => total + amount) / (todayWind.length));
-        const avTomorrowWind = Math.round(tomorrowWind.reduce((total, amount) => total + amount) / (tomorrowWind.length));
-        const avTomorrowPlus1Wind = Math.round(tomorrowPlus1Wind.reduce((total, amount) => total + amount) / (tomorrowPlus1Wind.length));
-
-
-        this.setState((prevState) => {
+        this.setState(prevState => {
+          prevState.datum = todayDatumStr;
           prevState.today.tempNow = tempNow;
           prevState.today.tempMax = todayTempMax;
           prevState.today.tempMin = todayTempMin;
@@ -136,45 +153,27 @@ class App extends Component {
           prevState.tomorrow.wind = avTomorrowWind;
           prevState.tomorrowPlus1.wind = avTomorrowPlus1Wind;
 
-
-
           //   // city
 
           return prevState;
-        })
+        });
 
         // .catch((error) => console.log(error));
       });
-
   }
-
-
-
-
-
-
 
   render() {
     return (
       <div>
-
-
-
-
         <Day
-          //today
+          datum={this.state.datum}
           TempNow={this.state.today.tempNow}
           TodayTempMax={this.state.today.tempMax}
           TodayTempMin={this.state.today.tempMin}
           TodayRain={this.state.today.rain}
           TodayWind={this.state.today.wind}
-
-
         />
-
-
-
-      </div >
+      </div>
     );
   }
 }
--- src/components/Day.js
+++ src/components/Day.js
@@ -1,53 +1,46 @@
-import React, { Component } from 'react';
-import Datum from './Date'
-import ArrowUp from 'react-icons/lib/fa/long-arrow-up';
-import ArrowDown from 'react-icons/lib/fa/long-arrow-down';
-import Moon from 'react-icons/lib/fa/moon-o';
+import React, { Component } from "react";
+import Datum from "./Datum";
+import ArrowUp from "react-icons/lib/fa/long-arrow-up";
+import ArrowDown from "react-icons/lib/fa/long-arrow-down";
+import Moon from "react-icons/lib/fa/moon-o";
 // import Arrow from "react-icons/lib/ti/arrow-forward";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Sidebar from './Menu'
-
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Sidebar from "./Menu";
 
 class Day extends Component {
+  render() {
+    return (
+      // <BrowserRouter>
 
+      //     <Sidebar />
+      //     <Switch>
 
-    render() {
-        return (
-            // <BrowserRouter>
+      //         <Route path='/' exact
 
-            //     <Sidebar />
-            //     <Switch>
+      <div>
+        {/* <Today {this.props.datum} /> */}
+        <div className="place">Enköping</div>
+        <div className="date">{this.props.datum}</div>
+        <div className="weatherinfo">
+          <Moon />
+          <div className="tempnow">
+            {this.props.TempNow}°<span className="deg">°</span>
+          </div>
+          <div className="temphighlow">
+            <ArrowUp /> {this.props.TodayTempMax}° <ArrowDown />{" "}
+            {this.props.TodayTempMin}°
+          </div>
+          <div className="wind">Vindstyrka: {this.props.TodayWind} m/s</div>
+          <div className="rain">Nederbörd: {this.props.TodayRain} mm</div>
+        </div>
+      </div>
+      //     )}
+      //     />
 
-            //         <Route path='/' exact 
-
-
-
-
-            <div>
-                {/* <Today {this.props.datum} /> */}
-                <div className='place'>Enköping</div>
-                <div className='date'>Torsdag, 26 april</div>
-                <div className='weatherinfo'>
-                    <Moon />
-                    <div className='tempnow'>{this.props.TempNow}°<span className='deg'>°</span></div>
-                    <div className='temphighlow'><ArrowUp /> {this.props.TodayTempMax}° <ArrowDown /> {this.props.TodayTempMin}°</div>
-                    <div className='wind'>Vindstyrka: {this.props.TodayWind} m/s</div>
-                    <div className='rain'>Nederbörd: {this.props.TodayRain} mm</div>
-                </div>
-            </div >
-            //     )}
-            //     />
-
-
-            //     </Switch>
-            // </BrowserRouter >
-
-        )
-    }
+      //     </Switch>
+      // </BrowserRouter >
+    );
+  }
 }
 
 export default Day;
-
-
-
