c5619f97fb92652c7ebff143f34d97432fdf2e3c
Max Elander <max.elander@mac.com>
2018-05-02 20:42:51 +0200

    Fixade bildstringen

--- src/components/Day.js
+++ src/components/Day.js
@@ -3,42 +3,60 @@ import ArrowUp from "react-icons/lib/fa/long-arrow-up";
 import ArrowDown from "react-icons/lib/fa/long-arrow-down";
 import Moon from "react-icons/lib/fa/moon-o";
 
-
-
-
 class Day extends Component {
-    render() {
-
-        const Icons =
-            ["", "sun.png", "sun.png", "suncloud.png", "suncloud.png", "cloud.png", "cloud.png",
-                "cloud.png", "rain.png", "rain.png", "rain.png", "thunder.png", "rain.png", "rain.png",
-                "rain.png", "snow.png", "snow.png", "snow.png", "rain.png", "rain.png", "rain.png",
-                "thunder.png", "rain.png", "rain.png", "rain.png", "snow.png", "snow.png", "snow.png"]
-
-
-
-        return (
-            <div>
-
-                <div className="place">Enköping</div>
-                <div className="date">{this.props.datum}</div>
-                <div className="weatherinfo">
-                    <Moon />
-                    <div>{this.props.IconNow}</div>
-                    {/* <p><img src={"./images/" + Icons[{ this.props.IconNow }] + ".png"} /> </p> */}
-                    <p><img src=`./images/${Icons[this.props.IconNow]}` /> </p>
-                    <div className="tempnow">{this.props.TempNow}°</div>
-                        <div className="temphighlow">
-                            <ArrowUp /> {this.props.TempMax}° <ArrowDown />{" "}
-                            {this.props.TempMin}°
+  render() {
+    const Icons = [
+      "",
+      "sun.png",
+      "sun.png",
+      "suncloud.png",
+      "suncloud.png",
+      "cloud.png",
+      "cloud.png",
+      "cloud.png",
+      "rain.png",
+      "rain.png",
+      "rain.png",
+      "thunder.png",
+      "rain.png",
+      "rain.png",
+      "rain.png",
+      "snow.png",
+      "snow.png",
+      "snow.png",
+      "rain.png",
+      "rain.png",
+      "rain.png",
+      "thunder.png",
+      "rain.png",
+      "rain.png",
+      "rain.png",
+      "snow.png",
+      "snow.png",
+      "snow.png"
+    ];
+    const weatherImage = "./images/" + Icons[this.props.IconNow];
+    return (
+      <div>
+        <div className="place">Enköping</div>
+        <div className="date">{this.props.datum}</div>
+        <div className="weatherinfo">
+          <Moon />
+          <div>{this.props.IconNow}</div>
+          {/* <p><img src={"./images/" + Icons[{ this.props.IconNow }] + ".png"} /> </p> */}
+          <p>
+            <img src={weatherImage} />
+          </p>
+          <div className="tempnow">{this.props.TempNow}°</div>
+          <div className="temphighlow">
+            <ArrowUp /> {this.props.TempMax}° <ArrowDown /> {this.props.TempMin}°
           </div>
-                        <div className="wind">Vindstyrka: {this.props.Wind} m/s</div>
-                        <div className="rain">Nederbörd: {this.props.Rain} mm</div>
-                </div>
-                </div >
+          <div className="wind">Vindstyrka: {this.props.Wind} m/s</div>
+          <div className="rain">Nederbörd: {this.props.Rain} mm</div>
+        </div>
+      </div>
+    );
+  }
+}
 
-                );
-            }
-        }
-        
-        export default Day;
+export default Day;
